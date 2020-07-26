ca557160a0a3f1120768731818060ac1f4f30ce2
Madhu112358 <42718457+Madhu112358@users.noreply.github.com>
2020-07-26 01:06:45 -0700

    chk if static is added

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 import psycopg2
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -131,9 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'static','image')
 STRIPE_PUBLISHABLE_KEY = 'pk_test_BCzj3veEjEz2NAaAfvMkTjGB0027B9TAeK'
