18d6c9cca3d17e3afcd2d61ed805cadfac180346
Madhu112358 <42718457+Madhu112358@users.noreply.github.com>
2020-08-08 20:44:03 -0700

    installed djangos3direct

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -135,15 +135,17 @@ django_heroku.settings(locals())
 
 
 #AWS
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = 'ecomm-madhu'
-AWS_S3_REGION_NAME = 'us-east-2'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_LOCATION = 'image'
+# MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+#MEDIA_ROOT = os.path.join(BASE_DIR,'static','image')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
+AWS_STORAGE_BUCKET_NAME = 'ecomm-madhu'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_ENDPOINT_URL = 'https://s3.amazonaws.com'
+S3DIRECT_DESTINATIONS = {
+    'primary_destination': {
+        'key': 'uploads/',
+    },
 }
-AWS_LOCATION = 'image'
-MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-MEDIA_ROOT = os.path.join(BASE_DIR,'static','image')
--- ecomm/urls.py
+++ ecomm/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static,settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('s3direct/', include('s3direct.urls')),
+]
 
 
