ab988b47b71a8cfdb71dfc0bfd7518f86dc749c1
Madhu112358 <42718457+Madhu112358@users.noreply.github.com>
2020-08-09 20:06:07 -0700

    settings

--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from s3direct.fields import S3DirectField
 
 #Model : Category
 class Category(models.Model):
@@ -27,8 +26,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-   # image = models.ImageField(upload_to='product', blank=True)
-    image = S3DirectField(dest='primary_destination', blank=True)
+    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
