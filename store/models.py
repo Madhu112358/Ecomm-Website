59df4e6f97bc57a19252d9105260caeeef603c0e
Madhu112358 <42718457+Madhu112358@users.noreply.github.com>
2020-08-09 20:55:20 -0700

    settings

--- store/models.py
+++ store/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     # image = models.ImageField(upload_to='product', blank=True)
-    image = models.FileField(blank=True)
+    image = models.ImageField(blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
